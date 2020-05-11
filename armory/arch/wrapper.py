382d3de1d22b7aae90f9cd13abe3f8558655f289
Tianji Cong <congtj@nagoya.eecs.umich.edu>
2020-05-10 22:06:58 -0400

    add tensorflow wrapper and config for armory

--- armory/arch/wrapper.py
+++ armory/arch/wrapper.py
@@ -2,44 +2,43 @@
 Wide-Resnet-34 Classifier for 32x32x3 CIFAR-10 Image Classification.
 Reference: https://github.com/twosixlabs/armory/blob/master/armory/baseline_models/pytorch/cifar.py
 """
+import os
+
 import numpy as np
-import torch
-import torch.nn as nn
-from armory.data.utils import maybe_download_weights_from_s3
-from art.classifiers import PyTorchClassifier
+import tensorflow as tf
+from armory import paths
+from art.classifiers import TensorFlowClassifier
 
-from arch.model import make_wideresnet_model
-
-
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from arch.model import make_madry_model
 
 
 def preprocessing_fn(img):
-    img = img.astype(np.float32) / 255.0
-    img = img.transpose(0, 3, 1, 2)  # from NHWC to NCHW
+    # img = img.astype(np.float32) / 255.0
+    # img = img.transpose(0, 3, 1, 2)  # from NHWC to NCHW
+    img = img.astype(np.float32)
+    # print(img.shape)
 
     return img
 
 
-def get_wideresnet(model_kwargs, wrapper_kwargs, weights_file=None):
-    model = make_wideresnet_model(**model_kwargs)
-    model.to(DEVICE)
+def get_madry_model(model_kwargs, wrapper_kwargs, weights_file=None):
+    model = make_madry_model(**model_kwargs)
+    saver = tf.train.Saver()
+    tf_sess = tf.Session()
 
-    if weights_file:
-        filepath = maybe_download_weights_from_s3(weights_file)
-        checkpoint = torch.load(filepath, map_location=DEVICE)
-        model.load_state_dict(checkpoint)
+    # Restore the checkpoint
+    saved_model_dir = paths.DockerPaths().saved_model_dir
+    filepath = os.path.join(saved_model_dir, weights_file)
+    model_file = tf.train.latest_checkpoint(filepath)
+    saver.restore(tf_sess, model_file)
 
-    wrapped_model = PyTorchClassifier(
-        model,
-        loss=nn.CrossEntropyLoss(),
-        optimizer=torch.optim.SGD(model.parameters(),
-                                  lr = 0.1,
-                                  momentum=0.9,
-                                  weight_decay=2e-4),
-        input_shape=(3, 32, 32),
-        nb_classes=10,
-        clip_values=(0.0, 1.0),
+    wrapped_model = TensorFlowClassifier(
+        input_ph=tf.placeholder(tf.float32, shape=[None, 32, 32, 3]),
+        output=model.pre_softmax,
+        labels_ph=tf.placeholder(tf.int64, shape=[None, 10]),
+        loss=model.xent,
+        sess=tf_sess,
+        clip_values=(0, 255),
         **wrapper_kwargs
     )
 
